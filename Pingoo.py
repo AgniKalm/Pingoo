f687649ace5fa3e9526b907851dadea317f59784
AgniKalm <123340164+AgniKalm@users.noreply.github.com>
2023-01-22 22:31:16 -0800

    Change test to google.com

--- Pingoo.py
+++ Pingoo.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/ping")
 def ping():
     try:
-        response = requests.get("https://www.yosdfutube.com")
+        response = requests.get("http://www.google.com")
         return "The shit is up!"
     except:
         return "The shit is down!"
